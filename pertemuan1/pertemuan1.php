bd0fbed63a4dce0a7b4eca0551f7d87479b2ddb4
dani081001 <62695233+dani081001@users.noreply.github.com>
2024-03-05 19:00:58 +0700

    latihan pertemuan 1

---
+++ pertemuan1/pertemuan1.php
@@ -0,0 +1,25 @@
+<?php 
+print"<pre>";
+print "<center><font size=14 color='magenta'>BIODATA</font></center>";
+echo "<hr size=4 width=60% color='blue'>";
+print "<center><font size=12 color='red'> Nama      : Muhammad Rahmadani </font><br></center>";
+print "<center><font size=12 color='red'> Email     : m.rahmadani.study@gmail.com </font><br></center>";
+print "<center><font size=12 color='red'> Jurusan   : Teknik Informatika </font><br></center>";
+print "<center><font size=12 color='red'> Hobby     : Touring </font><br></pre></center>";
+echo "<hr size=4 width=60% color='blue'>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
